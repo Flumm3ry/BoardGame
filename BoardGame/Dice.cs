0cc871171798fcbacfdfceb9080a9e55961d92df
Jeremy <jeremy.v.maxwell@bigpond.com>
2018-09-15 20:13:51 +1000

    added the dice class and added it to player

---
+++ BoardGame/Dice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardGame
+{
+	class Dice
+	{
+		private int _value;
+
+		private Random _rnd;
+
+		public Dice()
+		{
+			_rnd = new Random();
+		}
+
+		public int roll()
+		{
+			return _rnd.Next(1, 6);
+		}
+	}
+}
--- BoardGame/Player.cs
+++ BoardGame/Player.cs
@@ -9,12 +9,20 @@ namespace BoardGame
 		private string _name;
 		private int _position;
 		private int _speedMod;
+		private Dice[] _dice;
 
 		public Player(string name)
 		{
 			_name = name;
 			_position = 0;
 			_speedMod = 0;
+
+			_dice = new Dice[2];
+
+			for(int i=0; i < 2; i++)
+			{
+				_dice[i] = new Dice();
+			}
 		}
 
 		public void Move(int places, bool speedModApplied)
